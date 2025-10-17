f1f97193ddf93f9bf65b0c7561e3b54ae5e8e1d2
epadilla <daynet@interside.cl>
2025-10-17 00:56:48 -0300

    fix

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from stack_backend_s3.stack_backend_s3 import GenAiVirtualAssistantS3Stack
 from stack_backend_lambda_light_etl.stack_backend_lambda_light_etl import GenAiVirtualAssistantEtlLambdaStack
 from stack_backend_bedrock.stack_backend_bedrock import GenAiVirtualAssistantBedrockStack
 from stack_frontend_ddb_lambda.stack_frontend_ddb_lambda import GenAiVirtualAssistantDDBLambdaStack
-from stack_api_gateway_lambda.stack_api_gateway_lambda import GenAiVirtualAssistantApiGatewayStack
 #from stack_frontend_vpc_ecs_streamlit.stack_frontend_vpc_ecs_streamlit import GenAiVirtualAssistantVpcEcsStreamlitStack
 
 # AWS Settings 
@@ -41,22 +40,21 @@ bedrock_stack = GenAiVirtualAssistantBedrockStack(app,
                                                   input_lambda_fn_arn=ddb_stack.lambda_fn.function_arn,
                                                   input_s3_bucket_arn=s3_stack.bucket.bucket_arn)
 
-# API Gateway & Lambda Stack (now uses hardcoded Agent IDs)
-api_stackapi = GenAiVirtualAssistantApiGatewayStack(app,
-                                                  "GenAiVirtualAssistantApiGatewayStack",
-                                                  env=env_aws_settings)
-
+# Frontend Streamlit (ST)
+#st_stack = GenAiVirtualAssistantVpcEcsStreamlitStack(app,
+                                            #"GenAiVirtualAssistantVpcEcsStreamlitStack",
+                                            #env=env_aws_settings,
+                                            #input_metadata=env_context_params,
+                                            #input_ddb_table_arn=ddb_stack.table.table_arn                                            )
 
 # Hard Dependencies
 bedrock_stack.add_dependency(s3_stack)
 bedrock_stack.add_dependency(ddb_stack)
 etl_stack.add_dependency(s3_stack)
-# Note: API stack no longer depends on Bedrock stack since IDs are hardcoded
-# api_stackapi.add_dependency(bedrock_stack)
 #st_stack.add_dependency(ddb_stack)
 
 # Add Tags
-for stack in [s3_stack, etl_stack, bedrock_stack, ddb_stack, api_stackapi]:
+for stack in [s3_stack, etl_stack, bedrock_stack, ddb_stack]:
     Tags.of(stack).add("environment", env_name)
 
 app.synth()
